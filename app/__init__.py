cd0945bc7e8e8ed924cbf5928b03a8003a15e4db
rdobson94 <r_dobson@live.com>
2017-02-19 01:35:55 +0000

    Showing only JPGs

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Config Values
+USERNAME = 'admin'
+PASSWORD = 'password123'
+
+# SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
+SECRET_KEY = 'Sup3r$3cretkey'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = "app/static/uploads"
+from app import views
